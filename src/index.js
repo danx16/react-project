9cbd9190fd08a62e996c77f5c4188f38df2e86c2
userdan <root.danx20@gmail.com>
2020-04-17 12:11:34 +0800

    Inline styling with JSX in React JS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Components
+
+// Reason why they use "className" instead of "class" is to avoid collisions
+// class App extends React.Component
+// className="label"
+
 const App = () => { // const App = function()
     return(
-        <div>Hi, this is dan!</div>
+        <div>
+            <label for="name" class="label">Enter email</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'red', color:'white'}}>Submit</button>
+        </div>
     )
 }
 
