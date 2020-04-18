9c78c5eb2aca4ff21e11038b1ad137b91c4aef92
userdan <root.danx20@gmail.com>
2020-04-19 00:14:36 +0800

    Initial commit for Building Component

--- blog-posts/src/index.js
+++ blog-posts/src/index.js
@@ -2,15 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+const buttonText = 'Sample';
     return(
-        <div>
-            hello
+        <div className='ui comments'>
+            {/* comment */}
+            <div className='comment'>
+                <a href='/' className='avatar'>
+                    <img alt='profile picture'></img>
+                </a>
+            </div>
+            {/* content */}
+            <div className='content'>
+                <a href='/' className='author'>
+                    Sarah
+                </a>
+            </div>
+            {/* metadata */}
+            <div className='metadata'>
+                Today at 10:00PM
+            </div>
+            {/* text */}
+            <div className='text'>
+                It's amazing
+            </div>
         </div>
-    )
+    );
 }
 
-
-ReactDOM.render (
+ReactDOM.render(
     <App />,
     document.querySelector('#root')
 )
