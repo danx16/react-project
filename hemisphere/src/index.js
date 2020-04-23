1f4705412d3a5bc12afb2c664fd51356102b7b2a
userdan <root.danx20@gmail.com>
2020-04-23 21:53:31 +0800

    Getting users physical location

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import HemisphereDisplay from './HemisphereDisplay';
 
 const App = () => {
+    // Getting users physical location
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (error) => console.log(error)
+    );
+
     return (
         <div>
             <HemisphereDisplay />
