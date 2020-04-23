67633a9f1a34b151140aa3537259803f51a07255
userdan <root.danx20@gmail.com>
2020-04-23 23:13:05 +0800

    Removed functional component

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -2,41 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HemisphereDisplay from './HemisphereDisplay';
 
-/*  COMPARISON BETWEEN 
-    FUNCTIONAL COMPONENT 
-    AND 
-    CLASS-BASED COMPONENT
-*/
-
-
-// FUNCTIONAL COMPONENT
-const App = () => {
-    // Getting users physical location
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    )
-
-    return (
-        <div>
-            {/* <HemisphereDisplay /> */}
-            Latitude: 
-        </div>
-    )
-}
-
 // CLASS-BASED COMPONENT
-
 // Refactor App Component
 
-class App extends React.Component {
+class App extends React.Component { // It is allow us to put a ton of built in functionality from this other class called React Component into our class
+    
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (error) => console.log(error)
         );
         return(
-            <div>You are in the northern hemisphere</div>
+            <div>
+                You are in the northern hemisphere
+            </div>
         )
     }
 }
