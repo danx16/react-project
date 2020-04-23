89b625b8fc5f2c8297281dace9d7c92ac1401219
userdan <root.danx20@gmail.com>
2020-04-23 22:58:59 +0800

    Handling Async Operation in React,Transformation Functional Component to Class Based Component
    Used react state system and lifecycle methods

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -2,19 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HemisphereDisplay from './HemisphereDisplay';
 
+/*  COMPARISON BETWEEN 
+    FUNCTIONAL COMPONENT 
+    AND 
+    CLASS-BASED COMPONENT
+*/
+
+
+// FUNCTIONAL COMPONENT
+
 const App = () => {
     // Getting users physical location
     window.navigator.geolocation.getCurrentPosition(
         (position) => console.log(position),
         (error) => console.log(error)
-    );
+    )
 
     return (
         <div>
-            <HemisphereDisplay />
+            {/* <HemisphereDisplay /> */}
+            Latitude: 
         </div>
     )
 }
+
+// CLASS BASED COMPONENT
+// Refactor App Component
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (error) => console.log(error)
+        return(
+            <div>You are in the northern hemisphere</div>
+        )
+    }
+}
+
+
 
 ReactDOM.render(
     <App />,
