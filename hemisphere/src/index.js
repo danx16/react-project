f6ace054c3b1f19e141d93c327edffc61a6cd41c
userdan <root.danx20@gmail.com>
2020-05-02 13:23:06 +0800

    Conditional Rendering

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     constructor(props) {
         super(props)
 
-            this.state = { latitude:null, errorMessage: '' }
+            this.state = { latitude: null, errorMessage: '' }
             window.navigator.geolocation.getCurrentPosition(
                 (position) => {
                     this.setState({ latitude: position.coords.latitude})
@@ -21,12 +21,20 @@ class App extends React.Component {
     }
 
     render() {
-        return(
-            <div>
-                { this.state.latitude }
-                { this.state.errorMessage }
-            </div>
-        )
+        
+       if(!this.state.errorMessage && this.state.latitude){
+       return( <div>{ this.state.latitude }</div>)
+       }
+       if(this.state.errorMessage && !this.state.latitude) {
+        return( <div> {this.state.errorMessage} </div> )
+       }
+       else {
+           return(
+               <div>
+                   Loading...
+               </div>
+           )
+       }
     }
 }
 
