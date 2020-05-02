fe83cc2ca2d19aa2da699198e73f44a68c46d0b0
userdan <root.danx20@gmail.com>
2020-05-02 22:08:37 +0800

    Refactoring App with Lifecycle Method

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -1,43 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HemisphereDisplay from './HemisphereDisplay';
+// import HemisphereDisplay from './HemisphereDisplay';
 
 // CLASS-BASED COMPONENT
 // Refactor App Component
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
 
-            this.state = { latitude: null, errorMessage: '' }
-            window.navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    this.setState({ latitude: position.coords.latitude})
-                },
-            (error) => {
-                 this.setState({ errorMessage: error.message })
-            }
-        );
+    state = {latitude: null, errorMessage: ''}
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ latitude: position.coords.latitude})
+            },
+        (error) => {
+             this.setState({ errorMessage: error.message })
+        }
+    );
     }
 
-    render() {
-        
-       if(!this.state.errorMessage && this.state.latitude){
-       return( <div>{ this.state.latitude }</div>)
-       }
-       if(this.state.errorMessage && !this.state.latitude) {
-        return( <div> {this.state.errorMessage} </div> )
-       }
-       else {
-           return(
-               <div>
-                   Loading...
-               </div>
-           )
-       }
+
+    /* ComponentDidmount and ComponentDidUpdate */
+
+    // componentDidMount() {
+    //     console.log('componentDidMount')
+    // }
+    // componentDidUpdate() {
+    //     console.log('componentDidUpdate')
+    // }
+
+    render(){
+        if (!this.state.errorMessage && this.state.latitude) {
+            return(
+                <div> { this.state.latitude } </div>
+            )
+        }
+        if (this.state.errorMessage && !this.state.latitude) {
+            return(
+                <div> { this.state.errorMessage } </div>
+            )
+        }
+        else {
+            return(
+                <div> Loading... </div>
+            )
+        }
     }
+
 }
-
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
