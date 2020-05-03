eac196ba99044a0344a6b86bb3b7c482b774ddb1
userdan <root.danx20@gmail.com>
2020-05-03 16:28:50 +0800

    Deconstruction syntax

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-const HemisphereDisplay = (props) => {
+/* const HemisphereDisplay = (props) => {
     console.log(props.latitude);
     return (
         <div>Sample</div>
         )
+} */
+
+// Deconstruction syntax
+// Use specific property which is latitude instead of props
+// and call it as latitude instead of props.latitude
+
+const HemisphereDisplay = ({latitude}) => {
+    console.log(latitude);
+    return (
+        <div>Sample</div>
+        )
 }
+
 
 export default HemisphereDisplay;
