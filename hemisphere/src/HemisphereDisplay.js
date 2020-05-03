1b2b1bbc894bc70212010ebf16c67ef741426c18
userdan <root.danx20@gmail.com>
2020-05-03 22:33:36 +0800

    Styling the App

--- hemisphere/src/HemisphereDisplay.js
+++ hemisphere/src/HemisphereDisplay.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import '';
 import NorthernPic from './image/NorthernHemisphere.jpg';
 import SouthernPic from './image/SouthernHemisphere.jpg';
 
-const HemisphereDisplay = ({latitude}) => {
-
-    //Identical Ternary
-
-    const hemisphere = latitude > 0 ? 'Northern Hemisphere' : 'Southern Hemisphere'
-    const hemispherePic = latitude > 0 ? NorthernPic : SouthernPic
-
-    return (
-        <div>
-            { hemisphere }
-            <div>        
-                <img src={ hemispherePic } alt="" />
-            </div>
-        </div>
-        )
+const hemisphereConfig = {
+    Northern: {
+        text: 'It is northern hemisphere',
+        picture: NorthernPic
+    },
+    Southern: {
+        text: 'It is southern hemisphere',
+        picture: SouthernPic
+    }
 }
 
+const HemisphereDisplay = ({latitude}) => {
+
+    const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+    /* 
+        Reference back down inside of component hemisphere config square brackets inside of it.
+        Return object with a text and picture name inside of this object
+        Pull out that text and picture name 
+    */
+    const { text, picture } = hemisphereConfig[hemisphere] 
+
+    return (
+        <div className={ hemisphere } > 
+            <div className='ui raised text container segment'>
+                <div className='image'>
+                    <img src={picture} alt="Hemisphere Pic"/>
+                </div>
+                <div className='ui teal bottom attached label'>
+                    <h1> { text } </h1>
+                </div>
+            </div>  
+        </div>
+    )
+}
 
 export default HemisphereDisplay;
