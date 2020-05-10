15def9253e993bbf893cf6db75e794c7e550f6a5
userdan <root.danx20@gmail.com>
2020-05-10 11:38:52 +0800

    Initial commit for the image-list project

---
+++ image-list/src/components/SearchInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <input type="text">
+                        
+                    </input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
