4cff95847a47ba380255f3b183c3a23fde498fd3
userdan <root.danx20@gmail.com>
2020-05-10 22:42:00 +0800

    Async Await Function

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -5,8 +5,17 @@ import axios from 'axios';
 
 class App extends React.Component {
 
-    onSearchSubmit = (entry) => {
+   async onSearchSubmit(entry){
+
+
         console.log(entry);
+        
+        // Pull axios
+        const response = await axios.get(`https://pixabay.com/api/?key=16476138-de1a69ef4e3acf654b17bbcb6&q=${entry}&image_type=photo`)
+        // Use `` es2015 back ticks 
+        // key = 16476138-de1a69ef4e3acf654b17bbcb6 
+        // q = A URL encoded search term. If omitted, all images are returned. This value may not exceed 100 characters.
+        console.log(response.data.hits)
     }
 
     render() {
