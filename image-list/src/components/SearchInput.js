b0f74d4d6796206d6b75420a174ad14688b7c2a2
userdan <root.danx20@gmail.com>
2020-05-10 13:02:05 +0800

    Controlled vs Uncontrolled Elements

--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
+// Controlled
 class SearchInput extends React.Component {
-
-    // This onInputChange is a community convention
-    // Always put 
-    // on 
-    // Input - The element that we are assigning to callback
-    // Change - The event is change event it simply on input change
 
     onInputChange(event) {
         console.log(event.target.value);
@@ -18,16 +13,7 @@ class SearchInput extends React.Component {
                 <form action="" className='ui form'>
                     <div className='field'>
                         <div className='ui massive icon input'>
-                        <input 
-                            type="text" 
-                            placeholder='search...' 
-                            onChange={ this.onInputChange } // onSubmit, onClick
-/* 
-    NOTE: Do not put a set of parentheses  at the end of it when our component is rendered.
-    Right: onInputChange
-    Wrong: onInputChange()
-*/
-                        /> 
+                        <input type="text" placeholder='search...' onChange={ this.onInputChange } /> 
                         <i className='search icon'></i>
                         </div>
                     </div>
@@ -37,4 +23,33 @@ class SearchInput extends React.Component {
     }
 }
 
+/*
+
+// Uncontrolled
+
+class SearchInput extends React.Component {
+    
+    state = {entry: ''}
+
+    render() { // Rendered
+        return (
+            <div className='ui segment'>
+                <form action="" className='ui form'>
+                    <div className='field'>
+                        <div className='ui massive icon input'>
+                        <input 
+                        type="text" 
+                        placeholder='search...' 
+                        onChange={ (event) => this.setState({entry:event.target.value})}
+                        value={this.state.entry} 
+                        /> 
+                        <i className='search icon'></i>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+ */
 export default SearchInput;
