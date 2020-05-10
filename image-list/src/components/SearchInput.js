f003d20bbf061c5c98c66cd101a10f45afae5e11
userdan <root.danx20@gmail.com>
2020-05-10 12:45:33 +0800

    Creating event handlers. Handle event handlers. Note: Passed this callback function to a props on the input called onChange

--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 class SearchInput extends React.Component {
-    render() {
+
+    // This onInputChange is a community convention
+    // Always put 
+    // on 
+    // Input - The element that we are assigning to callback
+    // Change - The event is change event it simply on input change
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    render() { // Rendered
         return (
             <div className='ui segment'>
                 <form action="" className='ui form'>
                     <div className='field'>
                         <div className='ui massive icon input'>
-                        <input type="text" placeholder='search...' />
+                        <input 
+                            type="text" 
+                            placeholder='search...' 
+                            onChange={ this.onInputChange } // onSubmit, onClick
+/* 
+    NOTE: Do not put a set of parentheses  at the end of it when our component is rendered.
+    Right: onInputChange
+    Wrong: onInputChange()
+*/
+                        /> 
                         <i className='search icon'></i>
                         </div>
                     </div>
