8c0dfdcb127111017ae13a45466ae940e01ec031
userdan <root.danx20@gmail.com>
2020-05-10 23:31:37 +0800

    Setting the state = Data fetching inside of a React Application

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -4,24 +4,20 @@ import axios from 'axios';
 
 
 class App extends React.Component {
+    
+    state = { images:[] }
 
-   async onSearchSubmit(entry){
-
-
-        console.log(entry);
-        
-        // Pull axios
+    onSearchSubmit = async (entry) => {
         const response = await axios.get(`https://pixabay.com/api/?key=16476138-de1a69ef4e3acf654b17bbcb6&q=${entry}&image_type=photo`)
-        // Use `` es2015 back ticks 
-        // key = 16476138-de1a69ef4e3acf654b17bbcb6 
-        // q = A URL encoded search term. If omitted, all images are returned. This value may not exceed 100 characters.
-        console.log(response.data.hits)
+        console.log(this)  
+        this.setState({images:response.data.hits})
     }
 
     render() {
         return(
             <div className='ui container' style={{marginTop:'30px'}} >
             <SearchInput onSearchSubmit={this.onSearchSubmit} />
+            We have {this.state.images.length} images
             </div>
         )
     }
