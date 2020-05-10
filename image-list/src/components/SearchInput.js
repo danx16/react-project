73175439a4048825da3df712b12ab7b1cec1f958
userdan <root.danx20@gmail.com>
2020-05-10 19:58:42 +0800

    Understanding 'this' Keyword in JS

--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -3,17 +3,39 @@ import React from 'react';
 // Controlled
 class SearchInput extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value);
+    state={ entry:'' }
+
+    // Re-create a new version of function called onFormSubmit
+
+   /*  constructor(){
+        super()
+        this.onFormSubmit = this.onFormSubmit.bind(this)
+
+        OR
+
+        onFormSubmit = (event) => {
+        event.preventDefault() // prevent that behavior by calling event
+        console.log(this.state.entry)
+    }
+    }*/
+    
+    onFormSubmit = (event) => {
+        event.preventDefault() // prevent that behavior by calling event
+        console.log(this.state.entry)
     }
 
     render() { // Rendered
         return (
             <div className='ui segment'>
-                <form action="" className='ui form'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <div className='ui massive icon input'>
-                        <input type="text" placeholder='search...' onChange={ this.onInputChange } /> 
+                        <input 
+                            type="text" 
+                            placeholder='search...' 
+                            onChange={(event) => this.setState({entry:event.target.value})} 
+                            value={this.state.entry}
+                        /> 
                         <i className='search icon'></i>
                         </div>
                     </div>
