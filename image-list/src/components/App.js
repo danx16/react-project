039c4b232c7fb70df7fec02865af0b11eed64a62
userdan <root.danx20@gmail.com>
2020-05-11 18:52:38 +0800

    few update

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import SearchInput from './SearchInput';
-import axios from 'axios';
-import ImageList from './ImageList';
 
 
 class App extends React.Component {
     
     state = { images:[] }
 
-<<<<<<< HEAD
-   async onSearchSubmit (entry) {
-       const response = await axios.get(`https://pixabay.com/api/?key=16476138-de1a69ef4e3acf654b17bbcb6&q=${entry}&image_type=photo`);
-       console.log(response.data.hits)
-   
-=======
     onSearchSubmit = async (entry) => {
-        const response = await axios.get(`https://pixabay.com/api/?key=16476138-de1a69ef4e3acf654b17bbcb6&q=${entry}&image_type=photo`)
-        console.log(this)  
-        this.setState({images:response.data.hits})
->>>>>>> ff17e19d4983f01ebca049d913260a1be56da15c
+       const response = await axios.get(`https://pixabay.com/api/?key=16476138-de1a69ef4e3acf654b17bbcb6&q=${entry}&image_type=photo`);
+       this.setState({images:response.data.hits})
     }
 
     render() {
         return(
             <div className='ui container' style={{marginTop:'30px'}} >
-<<<<<<< HEAD
                 <SearchInput onSearchSubmit={this.onSearchSubmit} />
-=======
-            <SearchInput onSearchSubmit={this.onSearchSubmit} />
-            <ImageList images={this.state.images}/>
->>>>>>> ff17e19d4983f01ebca049d913260a1be56da15c
+                We have {this.state.images.length} images 
             </div>
         )
     }
