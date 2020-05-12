5597748e10b59cb04b1a0e864655df7efc0527e0
userdan <root.danx20@gmail.com>
2020-05-12 10:07:13 +0800

    Programmatic Redirects

--- react-router/src/components/Contact.js
+++ react-router/src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+    setTimeout( () => {
+        props.history.push('/about') 
+    }, 2000)
     return(
         <div className='ui raised very padded text container segment'
         style={{marginTop:'80px'}}
