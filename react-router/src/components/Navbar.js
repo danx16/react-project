9a14b53ee927c27b02a5f746747c5466c6e0ccd8
userdan <root.danx20@gmail.com>
2020-05-12 10:00:42 +0800

    Applying Link and Navlinks

--- react-router/src/components/Navbar.js
+++ react-router/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
 
 const Navbar = () => {
     return(
         <nav className='ui raised very padded segment'>
-            <a className='ui teal inverted segment'>Sample</a>
+            <a className='ui teal inverted segment' href="/">Sample</a>
             <div className='ui right float header'>
-                 <button className='ui button'><a href='/'>Home</a></button>
-                 <button className='ui button'><a href='/about'>About</a></button>
-                 <button className='ui button'><a href='/contact'>Contact</a></button>
+                 <button className='ui button'><NavLink to='/'>Home</NavLink></button>
+                 <button className='ui button'><Link to='/about'>About</Link></button>
+                 <button className='ui button'><Link to='/contact'>Contact</Link></button>
             </div>
         </nav>
     )
